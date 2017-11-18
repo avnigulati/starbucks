a7278cac6b941ceceb985f6a342572587392b29d
Avni Gulati <avnigulati@hotmail.com>
2017-11-18 14:38:13 -0800

    Update ProxyPatternTest.java

--- src/test/java/starbucks/ProxyPatternTest.java
+++ src/test/java/starbucks/ProxyPatternTest.java
@@ -1,6 +1,3 @@
-
-
-
 package starbucks ;
 
 
@@ -43,10 +40,15 @@ public class ProxyPatternTest
     {
         // Test Successful Pin:  
         // (enter correct pin digits)
-
-        // Validate Pin Succeeded 
+            app.touch(1, 5);
+            app.touch(2, 5);
+            app.touch(3, 5);
+            app.touch(4, 5);
+        // Validate Pin Succeeded
+            System.out.println("Second");
+            System.out.println(app.screen());
         // (replace with correct assert)
-        assertTrue( false ) ;
+        assertEquals("MyCards", app.screen() ) ;
     }
 
     @Test
@@ -54,10 +56,15 @@ public class ProxyPatternTest
     {
         // Test Failed Pin:  
         // (enter incorrect pin digits)
-
+            app.touch(1, 5);
+            app.touch(2, 5);
+            app.touch(3, 5);
+            app.touch(2, 6);
+            System.out.println("Firstg");
+            System.out.println(app.screen());
         // Validate Pin Succeeded 
         // (replace with correct assert)
-        assertTrue( false ) ;
+            assertEquals("PinScreen", app.screen() ) ;
     }
 
     
